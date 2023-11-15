543f69ab743821d380a14cda4905f30c4bb448d2
nevesgil <gilmarneves@outlook.com>
2023-11-15 16:54:37 -0300

    First commit

---
+++ todo_app/request.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=4)
+    description: str = Field(min_length=3, max_length=200)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "Develop",
+                "description": "Learn Scala programming",
+                "priority": 4,
+                "complete": False,
+            }
+        }
