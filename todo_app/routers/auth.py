a81439ce33412e0376e74d9469bbe100108a5599
nevesgil <gilmarneves@outlook.com>
2023-11-19 11:43:06 -0300

    Auth first commit

--- todo_app/routers/auth.py
+++ todo_app/routers/auth.py
@@ -1,9 +1,44 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter, Depends
+from todo_app.routers.auth_request import CreateUserRequest
+from database.models import Users
+from passlib.context import CryptContext
+from database.database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from starlette import status
 
 router = APIRouter()
 
-@router.get("/auth/")
-async def get_user():
-    return {"hello": "world"}
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# 
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True,
+    )
+
+    db.add(create_user_model)
+    db.commit()
+
+
+@router.post("/token")
+async def login_for_acess_token():
+    return 'token'
---
+++ todo_app/routers/auth_request.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+
+
+class CreateUserRequest(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "johndoe@example.com",
+                "username": "johndoe",
+                "first_name": "john",
+                "last_name": "doe",
+                "password": "mypassword",
+                "role": "admin",
+            }
+        }
