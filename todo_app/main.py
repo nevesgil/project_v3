5618b3876254f257e03a3dc44cd629d9f6c0873f
nevesgil <gilmarneves@outlook.com>
2023-11-19 18:02:33 -0300

    Set adjustments

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from database.database import engine, SessionLocal
 from database import models
-from todo_app.routers import auth, todos
+from todo_app.routers import auth, todos, admin, users
 
 app = FastAPI()
 
@@ -11,3 +11,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
--- todo_app/routers/auth.py
+++ todo_app/routers/auth.py
@@ -39,8 +39,8 @@ def authenticate_user(username: str, password: str, db):
     return user
 
 
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
-    encode = {"sub": username, "id": user_id}
+def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id, "role": role}
     expires = datetime.utcnow() + expires_delta
     encode.update({"exp": expires})
 
@@ -52,11 +52,12 @@ async def get_currrent_user(token: Annotated[str, Depends(oauth2_bearer)]):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
+        user_role: str = payload.get("role")
         if username is None or user_id is None:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED, detail="Not validated user"
             )
-        return {"username": username, "id": user_id}
+        return {"username": username, "id": user_id, "user_role": user_role}
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Not validated user"
@@ -89,6 +90,6 @@ async def login_for_acess_token(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Not validated user"
         )
 
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(user.username, user.id, user.role, timedelta(minutes=20))
 
     return {"access_token": token, "token_type": "bearer"}
