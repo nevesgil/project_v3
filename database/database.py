3bcbf0fd68f1439806bb277f9bed119e8a27766c
nevesgil <gilmarneves@outlook.com>
2023-11-19 21:34:48 -0300

    Changes for PostgreSQL

--- database/database.py
+++ database/database.py
@@ -2,10 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./database/todos_app.db"
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234senha@localhost/TodoApplicationDatabase'
+# old version
+# "sqlite:///./database/todos_app.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
+    # , connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
